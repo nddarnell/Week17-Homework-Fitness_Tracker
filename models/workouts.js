6eed2192527d62573c864ca7302f5579cd915cd8
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-06-27 18:19:29 -0700

    required items added to routes.js also added note, get and post routes need to be added in here

--- models/workouts.js
+++ models/workouts.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 var workoutSchema = new Schema({
     // update this what do you think should go in there?
+    
 //   image: { type: String, required: true },
 //   description: { type: String, required: true },
 //   rating: Number,
